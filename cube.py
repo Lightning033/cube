f2d6a2c0858b8305fc24838e0735f4e54d506a18
Lightning033 <75250464+Lightning033@users.noreply.github.com>
2022-07-21 17:44:24 +0530

    Add files via upload

---
+++ cube.py
@@ -0,0 +1,33 @@
+# cube.2
+
+import smartpy as sp
+
+class cube(sp.contract):
+def __init__(self,create):
+self.init()
+
+ @sp.entry_point
+    def start_cube(create):
+    sp.set(input black=1, white=0)
+
+    sp.verify(input)
+    winner_answer = (sp.now - sp.timestamp)
+
+
+
+
+    sp.if 
+    input == winning answer
+    sp.send(winner_answer, sp.balance) #winner
+    sp.else
+    sp.send()
+
+
+
+
+
+
+
+
+
+
